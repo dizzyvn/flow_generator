18aa538b19859b6ea7209adacf05fda91477ea86
Thien Q. Tran <thientquang@gmail.com>
2025-05-14 10:22:20 +0700

    don't execute code inside the flow anymore

--- src/flow.py
+++ src/flow.py
@@ -1,7 +1,6 @@
 from pocketflow import Flow
 
 from src.nodes.create_tools_doc import CreateToolsDoc
-from src.nodes.execute_code import ExecuteCode
 from src.nodes.generate_code import GenerateCode
 from src.nodes.setup_execution_sandbox import SetupExecutionSandbox
 
@@ -12,14 +11,8 @@ def create_flow():
     create_tools_doc_node = CreateToolsDoc()
     generate_code_node = GenerateCode(max_retries=3)
     setup_sandbox_node = SetupExecutionSandbox()
-    execute_code_node = ExecuteCode()
 
     # Connect nodes in sequence
-    (
-        create_tools_doc_node
-        >> generate_code_node
-        >> setup_sandbox_node
-        >> execute_code_node
-    )
+    (create_tools_doc_node >> generate_code_node >> setup_sandbox_node)
 
     return Flow(start=create_tools_doc_node)
